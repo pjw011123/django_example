47c4b51fd5585819e3824ffd0efb44519bfda60b
pjw011123 <pjw011123@hs.ac.kr>
2022-05-04 17:53:50 +0900

    111

--- first_app/views.py
+++ first_app/views.py
@@ -7,6 +7,7 @@ def index(request):...
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
+    print(1)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
