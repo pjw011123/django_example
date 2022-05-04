3c0b26b2a918fe78787db9a8004105953d1f9d81
pjw011123 <pjw011123@hs.ac.kr>
2022-05-04 17:51:28 +0900

    111

---
+++ first_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from first_app.models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
+# Register your models here.
---
+++ first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question, Choice
+from django.html import HttpResponseRedirect
+from django.urls import reverse
+
+def index(request):...
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'first_app/')
